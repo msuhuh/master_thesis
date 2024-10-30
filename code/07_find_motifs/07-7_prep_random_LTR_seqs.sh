75e0b217ff5900f015be96e5479d9e90baf22412
msuhuh <minna.sayehban@gmail.com>
2024-10-30 11:20:38 +0100

    testing resizing code

--- code/07_find_motifs/07-7_prep_random_LTR_seqs.sh
+++ code/07_find_motifs/07-7_prep_random_LTR_seqs.sh
@@ -1,30 +1,42 @@
+#!/bin/bash -l
+#SBATCH -A naiss2023-22-1038
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 5:00:00
+#SBATCH -J misa1224_prep_random_ltr_seq
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user minna.sayehban.1224@student.uu.se
+
+
 # IMPORT MODULES
 module load bioinfo-tools
+module load R_packages
 module load samtools
 module load BEDTools
 
 
 # SET VARIABLES
+size="+250"
 conversion_script="/proj/snic2020-16-24/private/users/minna/master_thesis/code/07_find_motifs/07-6_convert_GR_2_bed.R"
-LTR_vs_phyloP_GR="/proj/snic2020-16-24/private/users/minna/master_thesis/analyses/02_intersected_regions/hg38_LTR_vs_phyloP.RData"
-LTR_vs_phylop_bed="/proj/snic2020-16-24/private/users/minna/master_thesis/analyses/02_intersected_regions/hg38_LTR_vs_phyloP_local.bed"
+LTR_vs_phyloP_GR="/proj/snic2020-16-24/private/users/minna/master_thesis/analyses/02_intersected_regions/hg38.p12_LTR_vs_phyloP.RData"
+LTR_vs_phyloP_bed="/proj/snic2020-16-24/private/users/minna/master_thesis/analyses/03_K-mer_analysis/hg38.p12_LTR_vs_phyloP.bed"
 outpath="/proj/snic2020-16-24/private/users/minna/master_thesis/analyses/03_K-mer_analysis" 
-real_bed="${outpath}/real_regions_+50.bed" 
+real_bed="${outpath}/real_regions_${size}.bed" 
 chr_sizes="${outpath}/GRCh38.p12_chr_sizes"
 assembly="/proj/snic2020-16-24/private/users/minna/master_thesis/data/zoonomia_data/assembly/GCF_000001405.38_GRCh38.p12_genomic.fna"
-random_bed="${outpath}/random_LTR_regions_+50.bed"
-random_fasta="${outpath}/random_LTR_regions_+50.fa"
-lengths_file="${outpath}/real_region_lengths.bed"
-resized_LTR_vs_phylop_bed="/proj/snic2020-16-24/private/users/minna/master_thesis/analyses/02_intersected_regions/resized_hg38_LTR_vs_phyloP_local.bed"
+random_bed="${outpath}/random_LTR_regions_${size}.bed"
+random_fasta="${outpath}/random_LTR_regions_${size}.fa"
+lengths_file="${outpath}/real_region_lengths_${size}.bed"
+resized_LTR_vs_phylop_bed="/proj/snic2020-16-24/private/users/minna/master_thesis/analyses/03_K-mer_analysis/resized_hg38_LTR_vs_phyloP.bed"
 
 
 # CONVERT LTR_vs_PHYLOP GR-OBJECT TO BED
-# Rscript $conversion_script $LTR_vs_phyloP_GR $LTR_vs_phyloP_bed
+Rscript $conversion_script $LTR_vs_phyloP_GR $LTR_vs_phyloP_bed
 
 
 # RESIZE LTR REGIONS TO MATCH REAL REGIONS
-paste <(cut -f1,2 $LTR_vs_phylop_bed) <(awk '{print $2+$3}' $LTR_vs_phylop_bed | cut -f3) | \
-    awk 'NR==FNR{len[NR]=$1;next} {OFS="\t"; print $1, $2, $2+len[FNR]}' $lengths_file $LTR_vs_phylop_bed > ${resized_LTR_vs_phylop_bed}
+paste <(cut -f1,2 $LTR_vs_phylop_bed) <(awk '{print $1}' $lengths_file) | \
+    awk '{OFS="\t"; print $1, $2, $2 + $3}' > ${resized_LTR_vs_phylop_bed}
 
 
 # CREATE RANDOM REGIONS FILE IN BED FORMAT FROM LTR_vs_phyloP
